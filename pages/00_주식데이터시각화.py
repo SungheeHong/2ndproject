3ce31174892b0b4ce9deafee501101633c2194c9
SungheeHong <sungh22@hanmail.net>
2025-06-10 19:13:08 +0900

    Update 00_주식데이터시각화.py

--- pages/00_주식데이터시각화.py
+++ pages/00_주식데이터시각화.py
@@ -1,1 +1,13 @@
+companies = {
+    "Apple": "AAPL",
+    "Microsoft": "MSFT",
+    "Saudi Aramco": "2222.SR",  # 사우디 증시
+    "Alphabet (Google)": "GOOGL",
+    "Amazon": "AMZN",
+    "NVIDIA": "NVDA",
+    "Berkshire Hathaway": "BRK-B",
+    "Meta": "META",
+    "TSMC": "TSM",
+    "Tesla": "TSLA"
+}
 
